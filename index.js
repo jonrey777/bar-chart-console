45d563a37ae658be79c0274d406e590e57283aa3
Jon Rey Galera <jrg@newmediastaff.com>
2020-09-16 11:20:09 +0800

    refactored code

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 const draw = ({ label, value}) => {
-    let max = Math.max(...value);
-    let amount_size = value.length;
-    let label_size = label.length;
-    let output = result = label = "";
-    let tmp_max = max;
+  let max = Math.max(...value);
+  let amount_size = value.length;
+  let label_size = label.length;
+  let output = result = labels = "";
+  let tmp_max = max;
 
-    for (let i = 0; i <= max; i++) {
+  for (let i = 0; i <= max; i++) {
 
-        result=max - i + " ";
-        if((max - i) == 0) break;
-        for (let j = 0; j < amount_size; j++) {
-          
-          if(data.amount[j] >= tmp_max){
-            result+=" |||| ";
-          }else{
-            result+="      ";
-          }
+      result=max - i + " ";
+      if((max - i) == 0) break;
+      for (let j = 0; j < amount_size; j++) {
+        
+        if(value[j] >= tmp_max){
+          result+=" |||| ";
+        }else{
+          result+="      ";
         }
-      
-        tmp_max--;
-        output+=result+"\n";
-      
       }
-      for (let i = 0; i < label_size; i++) {
-        label+=data.label[i] + "   ";
-      }
+    
+      tmp_max--;
+      output+=result+"\n";
+    
+    }
+    for (let i = 0; i < label_size; i++) {
+      labels+=label[i] + "   ";
+    }
 
-      console.log(output + "  " + label);
+    console.log(output + "  " + labels);
 };
 
-module.exports = draw;
+module.exports = {draw};
